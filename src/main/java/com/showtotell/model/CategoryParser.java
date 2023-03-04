6f34d18bf82712d953f363c581e3d41bd6eaacc1
Sarah P <dop1300@rit.edu>
2023-03-04 17:21:33 -0500

    Added missing return statement.

--- src/main/java/com/showtotell/model/CategoryParser.java
+++ src/main/java/com/showtotell/model/CategoryParser.java
@@ -6,10 +6,10 @@ import java.util.Map;
 public class CategoryParser {
 
     public static String readFileContents(String path) {
-        
+
     }
 
     public static Map<String, Category> getCategories() {
-
+        return null;
     }
 }
