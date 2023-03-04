3870afce9a61bc17991244fe4a500c929abfbf74
saltyypanda <esh7943@rit.edu>
2023-03-04 17:12:11 -0500

    made button for gui

--- src/main/java/com/showtotell/view/ShowToTellGUI.java
+++ src/main/java/com/showtotell/view/ShowToTellGUI.java
@@ -1,14 +1,34 @@
 package com.showtotell.view;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class ShowToTellGUI extends Application {
+    private Button makeButton(Image image) {
+        Button button = new Button();
+        button.setBackground(new Background(new BackgroundImage(image,
+                        BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+        button.setPadding(Insets.EMPTY);
+        button.setPrefSize(500, 700);
+
+        button.setOnAction(new WhenClicked());
+        SquareChanger changer = new SquareChanger(view);
+        game.getSquare(row, col).setOnChange(changer);
+        return button;
+    }
+
+
     private GridPane makeImageCollage(Image one, Image two, Image three, Image four) {
         ImageView viewOne = new ImageView(one);
         ImageView viewTwo = new ImageView(two);
