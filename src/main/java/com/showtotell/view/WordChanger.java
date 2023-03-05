8947a0cf7996ec77dfe3f1cf2020b00df08f9096
saltyypanda <esh7943@rit.edu>
2023-03-04 23:45:32 -0500

    more gui edits

---
+++ src/main/java/com/showtotell/view/WordChanger.java
@@ -0,0 +1,5 @@
+package com.showtotell.view;
+
+public class WordChanger {
+    
+}
---
+++ src/main/java/com/showtotell/view/WordClicked.java
@@ -0,0 +1,19 @@
+package com.showtotell.view;
+
+import javafx.scene.media.MediaPlayer;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class WordClicked implements EventHandler<ActionEvent> {
+    private String filename;
+
+    public WordClicked(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        MediaPlay
+    }
+}
